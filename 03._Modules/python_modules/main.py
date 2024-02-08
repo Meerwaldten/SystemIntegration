c93cc7b76fbd7b744aeb6a30f6490d90f524a352
Mikkel Meerwaldt Jørgensen <Mikkel_m_j@hotmail.com>
2024-02-08 09:51:48 +0100

    Python modules added.

---
+++ 03._Modules/python_modules/main.py
@@ -0,0 +1,11 @@
+# En måde at gøre det på, mere effektivt og kortere
+from my_module import ClassA, ClassB
+
+ClassA()
+ClassB()
+
+# En anden måde at importe og integrere på, men den er lidt mere linje og karakter intensiv. 
+import my_module
+
+my_module.ClassA()
+my_module.ClassB()
