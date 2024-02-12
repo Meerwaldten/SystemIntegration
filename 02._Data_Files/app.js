6b164fc502238da77e50f9e8739fe6a9c15a25e3
Mikkel Meerwaldt Jørgensen <Mikkel_m_j@hotmail.com>
2024-02-12 17:54:32 +0100

    Updated my parser functions and made a backend server which fetches data from them

---
+++ 02._Data_Files/app.js
@@ -0,0 +1,39 @@
+import express from "express";
+import { readAndParseCSV, readAndParseJSON, readAndParseXML, readAndParseYAML, readAndParseTxt } from "./parser.js";
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send({ message: "Hello world"});    
+})
+
+app.get('/csv', async (req, res) => {
+    const csvData =  await readAndParseCSV();
+    res.send({ data: csvData });
+});
+
+
+app.get('/json', (req, res) => {
+    const jsonData = readAndParseJSON();
+    res.send({ data: jsonData });
+})
+
+app.get('/xml', (req, res) => {
+    const xmlData = readAndParseXML();
+    res.send({ data: xmlData });
+})
+
+app.get('/yaml', (req, res) =>{
+    const yamlData = readAndParseYAML();
+    res.send({ data: yamlData });
+})
+
+app.get('/txt', (req, res) => {
+    const txtData = readAndParseTxt();
+    res.send({ data: txtData });
+  });
+
+
+const PORT = "8080";
+
+app.listen(PORT, () => console.log("Server is running on: ", PORT));
--- 02._Data_Files/parser.js
+++ 02._Data_Files/parser.js
@@ -1,82 +1,75 @@
 // Imports
-const fs = require('fs');
-const csv = require('csv-parser');
-const xml2js = require('xml2js');
-const yaml = require('js-yaml');
+import fs from 'fs';
+import csvParser from 'csv-parser';
+import xml2js from 'xml2js';
+import yaml from 'js-yaml';
 
-
-// CSV read and parse 
-// CSV file path
+// Paths
 const csvPath = 'me.csv';
+const jsonPath = 'me.json';
+const xmlPath = 'me.xml';
+const yamlPath = 'me.yaml';
+const txtPath = 'me.txt';
 
-// Array to collect parsed CSV data
-const csvData = [];
-
-fs.createReadStream(csvPath)
-  .pipe(csv())
-  .on('data', (row) => {
-    csvData.push(row);
-  })
-  .on('end', () => {
-    console.log('Data parsed from CSV-File:', csvData);
-  })
-  .on('error', (err) => {
-    console.error('Error occurred while processing CSV file:', err);
+export function readAndParseCSV() {
+  return new Promise((resolve, reject) => {
+    const csvData = [];
+    fs.createReadStream(csvPath)
+      .pipe(csvParser())
+      .on('data', (row) => {
+        csvData.push(row);
+      })
+      .on('end', () => {
+        resolve(csvData);
+      })
+      .on('error', (err) => {
+        reject(err);
+      });
   });
+}
 
+export function readAndParseJSON() {
+  try {
+    const data = fs.readFileSync(jsonPath, 'utf8');
+    const jsonData = JSON.parse(data);
+    return jsonData;
+  } catch (error) {
+    throw error;
+  }
+}
 
-  // JSON read and parse
-  // JSON file path
-  const jsonPath = 'me.json';
-
-  fs.readFile(jsonPath, 'utf8', (err, data) => {
-    if (err) {
-      console.error('Error reading JSON file:', err);
-      return;
-    }
-    try {
-      const jsonData = JSON.parse(data);
-      //console.log('Name from json without parsing: ', jsonData.name);
-      // Example of the data is already in a format that can be read by Javascript.
-      console.log('Data parsed from JSON-File: ', jsonData);
-    } catch (error) {
-      console.error('Error parsing JSON data:', error);
-    }
-  });
-
-
-  // XML read and parse
-  // XML file path
-  const xmlPath = 'me.xml';
-
-  fs.readFile(xmlPath, 'utf8', (err, data) => {
-    if (err) {
-      console.error('Error reading XML file:', err);
-      return;
-    }
-    xml2js.parseString(data, { explicitArray: false }, (parseErr, result) => {
+export function readAndParseXML() {
+  try {
+    const data = fs.readFileSync(xmlPath, 'utf8');
+    let result;
+    xml2js.parseString(data, { explicitArray: false }, (parseErr, parsedResult) => {
       if (parseErr) {
-        console.error('Error parsing XML:', parseErr);
-        return;
+        throw parseErr;
       }
-      console.log('Data parsed from XML-File: ', result);
+      result = parsedResult;
     });
-  });
+    return result;
+  } catch (error) {
+    throw error;
+  }
+}
 
+export function readAndParseYAML() {
+  try {
+    const data = fs.readFileSync(yamlPath, 'utf8');
+    const yamlData = yaml.load(data);
+    return yamlData;
+  } catch (error) {
+    throw error;
+  }
+}
 
-  // YAML read and parse
-  // YAML file path
-  const yamlPath = 'me.yaml';
-
-  fs.readFile(yamlPath, 'utf8', (err, data) => {
-    if (err) {
-      console.error('Error reading YAML file:', err);
-      return;
-    }
-    try {
-      const yamlData = yaml.load(data);
-      console.log('Data parsed from YAML-File: ',yamlData);
-    } catch (parseErr) {
-      console.error('Error parsing YAML:', parseErr);
-    }
-  });
+export function readAndParseTxt() {
+  try {
+    const data = fs.readFileSync(txtPath, 'utf8');
+    const parsedData = yaml.load(data);
+    return parsedData;
+  } catch (error) {
+    throw error;
+  }
+}
