6231e4f20b846471916e51cb7825d7ee2e33b895
Mikkel Meerwaldt Jørgensen <Mikkel_m_j@hotmail.com>
2024-04-04 12:22:02 +0200

    Last part of april 4ths lecture

---
+++ 17._Multipart-Forms/python-multipart-forms/main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI, Form
+
+
+app = FastAPI()
+
+
+@app.post('/form')
+def basic_form(username: str = Form(...), password: str = Form(default=..., min_length = 8)):
+    print(username)
+    return {}
